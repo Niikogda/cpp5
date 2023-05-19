55197d04c134edbeb0bdce1910e2ec3ecbb7a71a
Niikogda <niikogda@gmail.com>
2023-05-19 19:09:36 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
- 
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int SIZE = 5;
+    int arr[SIZE];
+
+    cout << "Введіть елементи масиву:" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Елемент " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Масив у зворотньому порядку:" << endl;
+    for (int i = SIZE - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
