24089db3467680935842d0250cb2fd574c7ee955
Niikogda <niikogda@gmail.com>
2023-05-19 19:17:23 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
- 
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int NUM_SIDES = 5;
+    double sides[NUM_SIDES];
+
+    cout << "Введіть довжини сторін п'ятикутника:" << endl;
+    for (int i = 0; i < NUM_SIDES; i++) {
+        cout << "Сторона " << i + 1 << ": ";
+        cin >> sides[i];
+    }
+
+    double perimeter = 0;
+    for (int i = 0; i < NUM_SIDES; i++) {
+        perimeter += sides[i];
+    }
+
+    cout << "Периметр п'ятикутника: " << perimeter << endl;
+
+    return 0;
+}
